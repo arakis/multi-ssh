81a427597e8d89cf5c8ac9e0048fa2a83e73cbc3
Sebastian Loncar <sebastian.loncar@gmail.com>
2025-03-20 22:24:57 +0100

    na

--- multi-ssh-completion.bash
+++ multi-ssh-completion.bash
@@ -5,11 +5,11 @@ _multi_ssh_completion() {
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
-    opts="--syncronize-panes --send-keys --exec --local-session --remote-session --remote-user --completion"
+    opts="--syncronize-panes --send-keys --exec --local-session --remote-session --remote-user --ssh-user --ssh-key --completion"
 
     # Handle all cases
     case "$prev" in
-        --local-session|--remote-session|--remote-user)
+        --local-session|--remote-session|--remote-user|--ssh-user|--ssh-key)
             # These options expect an argument, don't complete anything
             return 0
             ;;
